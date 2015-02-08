40d658c7cc778dbceef596113fe423792b28857f
Kaiyuan <sealour@hotmail.com>
2015-02-08 10:23:30 +0800

    制作完成页面
    正式的页面版面第一版完成。

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 jQuery(document).ready(function() {
-	var projectName = 'Change Logs';
-	var projectAlias = 'ChangeLogs';
+	if (!window.projectName) {
+		// 项目名称
+		var projectName = 'Change Logs';
+	};
+	if (!window.projectAlias) {
+		// 项目 localStorage 名
+		var projectAlias = 'ChangeLogs';
+	};
+	if (!window.indexJson) {
+		// logs 列表
+		var indexJson = 'index.json';
+	};
+
+	// 项目 URL
 	var projectURL = window.location.origin+window.location.pathname;
-	// var logAlias = window.location.hash; //获取 #!
-	// if (logAlias) {
-	// 	logAlias = logAlias.substr(2);
-	// } else {
-	// 	logAlias = false;
-	// };
-	var indexJson = 'index.json';
-	var Main = $('#Main');
-	var indeBn = $('#index-bn');
+	var MainBox = $('#Main');	// 内容 DOM
+	var Main = document.getElementById('Main');
+	var indeBn = $('#index-bn');	// 目录按钮
 
 	// 列出 localStorage 内容
 	function listsData () {
@@ -25,46 +31,56 @@ jQuery(document).ready(function() {
 				logsPost += '</div>';
 			};
 		};
-		Main.fadeOut('fast', function() {
-			document.getElementById('Main').innerHTML = logsPost;
+
+		// 修改内容，先隐藏
+		MainBox.fadeOut('fast', function() {
+			// 改变内容
+			Main.innerHTML = logsPost;
+			$(this).fadeIn('fast');	// 显示内容
 			console.log('列出目录');
-			$(this).fadeIn('fast');
 		});
 	}
+
+	// 处理列表
 	function catList (carArray) {
 		var Category = '<ul class="Category">'
 		for (var i = 0; i < carArray.length; i++) {
-			Category += '<li><a href="'+projectURL+'#!'+carArray[i].alias+'">'+carArray[i].name+'</a></li>'
+			Category += '<li><a href="'+projectURL+'#!'+encodeURIComponent(carArray[i].name)+'">'+carArray[i].name+'</a></li>'
 		};
 		Category += '<ul>';
 		return(Category);
 	}
 
-	// 
-	function loadData (loadAlias) {
+	// 遍历数据获取对应的路径并加载
+	function loadData (loadAlias,notFirst) {
+		// 读取数据
 		var localData = JSON.parse(localStorage[projectAlias]);
 		loadLength = localData.length;
-		for (var i = 0; i < loadLength+1; i++) {
-			if (i < loadLength) {
-				for (var i2 = 0; i2 < localData[i].list.length; i2++) {
-					console.log(localData[i].list[i2].alias+','+loadAlias);
-					if(localData[i].list[i2].alias === loadAlias) {
-						console.log('找到 %s', loadAlias);
-						loadLog(localData[i].list[i2].path, localData[i].list[i2].name);
-						console.log('输出 %s', loadAlias);
-						break;
-					};
+		for (var i = 0; i < loadLength; i++) {
+			for (var i2 = 0; i2 < localData[i].list.length; i2++) {
+				if(localData[i].list[i2].name === loadAlias) {
+					loadLog(localData[i].list[i2].path, localData[i].list[i2].name);
+					console.log('找到并输出 %s', loadAlias);
+					break;
+				} else if (i === loadLength-1){
+					Main.innerHTML = '<div class="error center">没有在数据库中找到该项目</div>';
+					console.log('数据中没有 %s', loadAlias);
 				};
-			} else{
-				document.getElementById('Main').innerHTML = '没有在数据库中找到该项目';
 			};
 		};
 	}
+
+	// 显示项目 Log
 	function loadLog (logPath,logName) {
+		/*
+		*	logPath -> 需要加载 log 的路径
+		*	logName -> log 名
+		*/ 
 		var logPost = '<h2>'+logName+'</h2><div id="Log-Path" class="fz12"><a href="'+projectURL+logPath+'">'+projectURL+logPath+'</a></div>';
 		// console.log(logPath);
 		$.ajax({
 			url: logPath,
+			cache: false,
 			dataType: 'json',
 			success: function (logData) {
 				logPost += '<ul id="Log-Main" class="main-box-list left100">';
@@ -72,8 +88,8 @@ jQuery(document).ready(function() {
 					logPost += '<li class="left100"><button class="log-header left100"><h3>'+logData[i].version+'</h3><time class="fz12 c777">'+logData[i].time+'</time></button><div class="post left100">'+marked(logData[i].log)+'</div></li>';
 				};
 				logPost += '</ul>'
-				Main.fadeOut('fast', function() {
-					document.getElementById('Main').innerHTML = logPost;
+				MainBox.fadeOut('fast', function() {
+					Main.innerHTML = logPost;
 					console.log("将 %s 内容输出页面",logPath);
 					$(this).fadeIn('fast');
 				});
@@ -86,12 +102,12 @@ jQuery(document).ready(function() {
 	}
 	
 	// 点击 index 按钮列出所有 logs
-	indeBn.click(function(event) {
+	indeBn.click(function() {
 		if (window.location.hash) {
+			$("html,body").animate({scrollTop: '0'}, 500);
+			listsData();
+			// 修改 URL
 			window.history.pushState(null, projectName, ' ');
-			$("html,body").animate({scrollTop: '0'}, 500,function () {
-				listsData();
-			});
 		};
 	});
 
@@ -102,7 +118,9 @@ jQuery(document).ready(function() {
     	var loadAlias = href.substring(href.lastIndexOf("#!"));    // 获取 URL 问号之后的字符
     	if (loadAlias) {  
             //如果 PageID 存在运行下面的内容
-    		loadAlias = loadAlias.substr(2); //截取前面的问号
+    		loadAlias = decodeURIComponent(loadAlias.substr(2)); //截取前面的问号
+    		var nowTitle = loadAlias + ' - ' + projectName;
+    		window.history.pushState(null, nowTitle, '#!'+loadAlias);
     		loadData(loadAlias); //载入页面内容，后面的 'go' 
             return false; //阻止a标签跳转
     	};
@@ -114,6 +132,31 @@ jQuery(document).ready(function() {
 		$('#Log-Main>li:nth-child('+thisVer+')>.post').toggle('fast');
 	});
 
+	// 将 index.json 写入 localStorage
+	function theFirst (argument) {
+		$.ajax({
+			url: indexJson,
+			dataType: 'text',
+			success: function (indexData) {
+				localStorage[projectAlias] = indexData;
+				console.log("将 %s 写入 localStorage",indexJson);
+	            var logAlias = window.location.hash; //重新获取对应的值
+				if (logAlias) {
+					logAlias = decodeURIComponent(logAlias.substr(2));
+					console.log("当前文档别 %s",logAlias);
+	            	loadData(logAlias);
+				} else {
+	            	listsData();
+				};
+				
+			},
+			error: function () {
+				console.log("无法读取 %s",indexJson);
+			}
+		});
+	}
+	theFirst();
+
     /*  判断 url 载入内容，有下面的 window.addEventListener 就不需要这里了!
     *   但是 Firefox 和 IE 不能直接用 window.addEventListener 载入内容所以判断一下浏览器。
     *   navigator.userAgent.match("MSIE")||navigator.userAgent.match("Firefox")
@@ -121,8 +164,8 @@ jQuery(document).ready(function() {
 	if (navigator.userAgent.match("MSIE")||navigator.userAgent.match("Firefox")) {
 		logAlias = window.location.hash;
 		if (logAlias) {
-			logAlias = logAlias.substr(2);
-			console.log("当前文档别名 %s",logAlias);
+			logAlias = decodeURIComponent(logAlias.substr(2));
+			console.log("当前文档名 %s",logAlias);
             loadData(logAlias);
 		} else {
             listsData();
@@ -135,8 +178,8 @@ jQuery(document).ready(function() {
 		if (history.pushState){
             var logAlias = window.location.hash; //重新获取对应的值
 			if (logAlias) {
-				logAlias = logAlias.substr(2);
-				console.log("当前文档别名 %s",logAlias);
+				logAlias = decodeURIComponent(logAlias.substr(2));
+				console.log("当前文档别 %s",logAlias);
             	loadData(logAlias);
 			} else {
             	listsData();
@@ -144,26 +187,5 @@ jQuery(document).ready(function() {
 		}
 	}, false);
 
-	// 将 index.json 写入 localStorage
-	$.ajax({
-		url: indexJson,
-		dataType: 'text',
-		success: function (indexData) {
-			localStorage[projectAlias] = indexData;
-			console.log("将 %s 写入 localStorage",indexJson);
-            var logAlias = window.location.hash; //重新获取对应的值
-			if (logAlias) {
-				logAlias = logAlias.substr(2);
-				console.log("当前文档别名 %s",logAlias);
-            	loadData(logAlias);
-			} else {
-            	listsData();
-			};
-			
-		},
-		error: function () {
-			console.log("无法读取 %s",indexJson);
-		}
-	});
 
 });
